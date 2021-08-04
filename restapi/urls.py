9928bc0418e5220b6d78e69c4b6b32db04dc0c13
Boburjon <bobur.ermonov.97@mail.ru>
2021-08-04 17:22:03 +0500

    jwt qo'shildi

--- restapi/urls.py
+++ restapi/urls.py
@@ -15,12 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+
+from users.views import UserAPIView
 from .views import home
+from rest_framework_simplejwt.views import (
+TokenObtainPairView,
+TokenRefreshView,
+
+)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('api.urls')),
-    path('',home)
-
-]
+    path('',home),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/user/', UserAPIView.as_view(), name='user'),]
